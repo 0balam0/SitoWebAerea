b30b7db91c8d7d643e5edec2833bd5af5d92d635
Matteo De Marco <dmr.matteo@gmail.com>
2025-04-13 09:31:01 +0200

    fixed the error on display cadr product, fixed the error on the resize design your tools

--- designYourTools.php
+++ designYourTools.php
@@ -36,6 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="it">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Richiesta Attrezzo Aereo</title>
   
     <link rel="stylesheet" href="designTool.css"> <!-- Collega il tuo file CSS -->
--- header.php
+++ header.php
@@ -1,4 +1,44 @@
 <header>
+  <style>
+      @media (max-width: 800px) {
+    .hamburgerDiv{
+        display:flex;
+        justify-content: space-between;
+    }
+    .hamburger {
+      display: flex;
+      justify-content: center;
+    }
+  
+    .navDiv {
+      display: none;
+      position: absolute;
+      top: 70px;
+      margin-left: 10px;
+      background: white;
+      box-shadow: 0 0 10px rgba(0,0,0,0.1);
+      border-radius: 8px;
+      padding: 10px;
+      z-index: 10;
+    }
+    .headerIcone{
+        display: none;
+    }
+
+    .navDiv.active {
+      display: block;
+      background-color: var(--colorHamburgerBK);
+    }
+  
+    .navDiv ul {
+      flex-direction: column;
+      gap: 10px;
+    }
+    .headerButton{
+        display:none;
+    }
+  }
+    </style>
   <div class='headerIcone'>
     <img src="images/icone/iconaBase.svg" alt="Site icone" style='width: 100px;' >
   </div>
